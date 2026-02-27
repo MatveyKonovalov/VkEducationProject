f0b1e62792a964226d0503400687a2874b9b7bb7
MatveyKonovalov <fj.konovalov@yandex.ru>
2026-02-27 23:01:29 +0300

    third commit CTRL+K

--- app/src/main/java/com/example/vkeducationproject/MainActivity.kt
+++ app/src/main/java/com/example/vkeducationproject/MainActivity.kt
@@ -13,12 +13,29 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.vkeducationproject.ui.theme.VkEducationProjectTheme
 import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-                Text("Hello Android")
+            Column {
+                Text(
+                    "Hello Android",
+                    modifier = Modifier.padding(100.dp)
+                )
+                Row {
+                    for (el in 1..5){
+                        Box(modifier = Modifier
+                            .padding(10.dp)
+                            .size(30.dp)
+                            .background(Color.Black))
+                    }
+
+
+                }
             }
         }
     }
